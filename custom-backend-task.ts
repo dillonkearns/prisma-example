b039515c7f305dcde998b5ddde2a4dac37218787
Dillon Kearns <dillon@incrementalelm.com>
2023-01-26 11:51:26 -0800

    Try prisma setup.

--- custom-backend-task.ts
+++ custom-backend-task.ts
@@ -1,3 +1,14 @@
-export async function hello(name) {
-  return `Hello ${name}!`;
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function users() {
+  try {
+    const users = await prisma.user.findMany({
+      include: { profile: true },
+    });
+    return users;
+  } catch (error) {
+    console.trace(error);
+    return ["PRISMA ERROR"];
+  }
 }
